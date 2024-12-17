5fdc0960f3f2bc3223d6e73e3aa5e02c319f7663
Carlos Joubert <carlosjoubert@gmail.com>
2024-12-17 13:41:24 -0300

    modelagem do BD, relacionamento de tabelas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,28 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    @OneToMany(mappedBy = "missoes")
+    private NinjaModel ninja;
+
+    public NinjaModel getNinja() {
+        return ninja;
+    }
+
+    public void setNinja(NinjaModel ninja) {
+        this.ninja = ninja;
+    }
+
+
+}
